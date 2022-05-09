7a456475005b89ebc650f483e01f4641967c0037
Leda <ledathemis@protonmail.com>
2022-05-09 12:29:27 +0000

    add Layout to render navbar with other components

--- src/RouterSwitch.js
+++ src/RouterSwitch.js
@@ -1,13 +1,25 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
 import App from './App';
+import Nav from './components/Nav';
 
 const RouterSwitch = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<App />} />
+        </Route>
       </Routes>
     </BrowserRouter>
+  );
+};
+
+const Layout = () => {
+  return (
+    <div id="layout">
+      <Nav />
+      <Outlet />
+    </div>
   );
 };
 
