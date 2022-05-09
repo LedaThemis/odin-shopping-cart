2fb2874cb8e710dd95a2d7a290ab792e3dac369f
Leda <ledathemis@protonmail.com>
2022-05-09 13:51:58 +0000

    add ShopItem component

---
+++ src/components/ShopItem.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import '../styles/ShopItem.css';
+
+const ShopItem = ({ id, title, price, imageURL, added }) => {
+  const [itemCount, setItemCount] = useState(0);
+
+  const onInputChange = (e) => {
+    setItemCount(e.target.value);
+  };
+
+  const incrementItemCount = () => {
+    setItemCount(itemCount + 1);
+  };
+  const decrementItemCount = () => {
+    setItemCount(itemCount - 1);
+  };
+
+  return (
+    <div className="shop--item">
+      <div className="shop--item--top">
+        <img className="shop--item--image" src={imageURL} alt=""></img>
+        <p className="shop--item--title">{title}</p>
+      </div>
+      <div className="shop--item--bottom">
+        <p className="shop--item--price">{price}</p>
+        <div className="shop--item--controls">
+          <input className="shop--item--input" type="number" min="1" onChange={onInputChange} value={itemCount}></input>
+          <button className="shop--item--increment shop--item--button" onClick={incrementItemCount}>
+            +
+          </button>
+          <button className="shop--item--decrement shop--item--button" onClick={decrementItemCount}>
+            -
+          </button>
+          <button className={'shop--item--add shop--item--button' + (added ? ' shop--item--added' : '')}>
+            Add To Cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ShopItem;
