7031d382acf193703e1afed004e0186294a098be
Leda <ledathemis@protonmail.com>
2022-05-09 13:51:43 +0000

    add method to fetch Shop items from API, render items

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,23 +1,35 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import '../styles/Shop.css';
+import ShopItem from './ShopItem';
 
 const Shop = () => {
+  const [itemsData, setItemsData] = useState([]);
+
   const fetchProducts = async () => {
     const fetchedData = await fetch('https://fakestoreapi.com/products/');
     const fetchedDataJSON = await fetchedData.json();
     return fetchedDataJSON;
   };
 
+  const processFetchedData = (dataArray) => {
+    return dataArray.map(({ id, title, price, image }) => ({ id: id, title: title, price: price, imageURL: image }));
+  };
+
   useEffect(() => {
     (async () => {
       const fetchedData = await fetchProducts();
-      console.log(fetchedData);
+      setItemsData(processFetchedData(fetchedData));
     })();
   }, []);
 
   return (
     <div className="shop">
       <h2>Shop</h2>
+      <div className="shop--items">
+        {itemsData.map(({ id, title, price, imageURL }) => (
+          <ShopItem key={id} id={id} title={title} price={price} imageURL={imageURL} />
+        ))}
+      </div>
     </div>
   );
 };
