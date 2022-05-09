593eb5f46aa2da48b29aaf3dfc1bf83082b73197
Leda <ledathemis@protonmail.com>
2022-05-09 12:44:16 +0000

    add welcoming text, style home

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,12 @@
+import './Home.css';
+
 function Home() {
-  return <div className="Home">Home</div>;
+  return (
+    <div className="Home">
+      <h2 className="welcome--header">Welcome to Our Shop!</h2>
+      <p>We Hope you find what you're looking for.</p>
+    </div>
+  );
 }
 
 export default Home;
