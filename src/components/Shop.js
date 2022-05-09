01f2e5abd46834356ae5f3d5d5315e515cfd0706
Leda <ledathemis@protonmail.com>
2022-05-09 14:58:40 +0000

    add method to handle add item to cart

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,9 +1,18 @@
 import { useState, useEffect } from 'react';
 import '../styles/Shop.css';
-import ShopItem from './ShopItem';
+
+import { Outlet } from 'react-router-dom';
 
 const Shop = () => {
   const [itemsData, setItemsData] = useState([]);
+  const [shoppingCartItems, setShoppingCartItems] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const fetchedData = await fetchProducts();
+      setItemsData(processFetchedData(fetchedData));
+    })();
+  }, []);
 
   const fetchProducts = async () => {
     const fetchedData = await fetch('https://fakestoreapi.com/products/');
@@ -15,21 +24,39 @@ const Shop = () => {
     return dataArray.map(({ id, title, price, image }) => ({ id: id, title: title, price: price, imageURL: image }));
   };
 
-  useEffect(() => {
-    (async () => {
-      const fetchedData = await fetchProducts();
-      setItemsData(processFetchedData(fetchedData));
-    })();
-  }, []);
+  const isInCart = (itemID) => {
+    return shoppingCartItems.find(({ id }) => itemID === id);
+  };
+
+  const getItem = (itemID) => {
+    return itemsData.find(({ id }) => id === itemID);
+  };
+
+  const removeItemAtIndex = (arr, index) => {
+    return arr.slice(0, index).concat(arr.slice(index + 1));
+  };
+
+  const addItemToCart = (itemID, itemCount) => {
+    setShoppingCartItems(shoppingCartItems.concat({ id: itemID, itemCount: itemCount }));
+  };
+
+  const removeItemFromCart = (itemID) => {
+    const itemIndex = shoppingCartItems.findIndex(({ id }) => id === itemID);
+    setShoppingCartItems(removeItemAtIndex(shoppingCartItems, itemIndex));
+  };
+
+  const handleAddToCart = (itemID, itemCount) => {
+    if (isInCart(itemID)) {
+      removeItemFromCart(itemID);
+    } else {
+      addItemToCart(itemID, itemCount);
+    }
+  };
 
   return (
     <div className="shop">
       <h2>Shop</h2>
-      <div className="shop--items">
-        {itemsData.map(({ id, title, price, imageURL }) => (
-          <ShopItem key={id} id={id} title={title} price={price} imageURL={imageURL} />
-        ))}
-      </div>
+      <Outlet context={{ itemsData, shoppingCartItems, isInCart, handleAddToCart, getItem }} />
     </div>
   );
 };
