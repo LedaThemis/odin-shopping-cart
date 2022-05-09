65bc346fd448d9b2b5a6cf3e1f5e099b7ef996bd
Leda <ledathemis@protonmail.com>
2022-05-09 12:06:23 +0000

    configure RouterSwitch

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import RouterSwitch from './RouterSwitch';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterSwitch />
   </React.StrictMode>
 );
