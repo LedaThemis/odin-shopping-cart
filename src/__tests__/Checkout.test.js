6931006efe4d35dd8d7231badd9613b8494e2e22
Leda <ledathemis@protonmail.com>
2022-05-10 06:41:08 +0000

    fix tests by providing context

--- src/__tests__/Checkout.test.js
+++ src/__tests__/Checkout.test.js
@@ -3,27 +3,14 @@ import '@testing-library/jest-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Checkout from '../components/Checkout';
 
-beforeEach(() => {
-  jest.resetModules();
-});
-
-jest.mock('react-router-dom', () => {
-  const originalModule = jest.requireActual('react-router-dom');
-
-  const outletDataMock = { shoppingCartItems: [], getItem: () => {} };
-  const useOutletContextMock = () => outletDataMock;
-
-  return {
-    __esModule: true,
-    ...originalModule,
-    useOutletContext: useOutletContextMock,
-  };
-});
+import { ShopOutletContext } from '../components/ShopOutletContext';
 
 test('should display "You currently have no items." when no items are provided', () => {
   render(
     <BrowserRouter>
-      <Checkout />
+      <ShopOutletContext.Provider value={{ shoppingCartItems: [], getItem: () => {} }}>
+        <Checkout />
+      </ShopOutletContext.Provider>
     </BrowserRouter>
   );
 
@@ -33,7 +20,9 @@ test('should display "You currently have no items." when no items are provided',
 test('should contain link to shop page', () => {
   render(
     <BrowserRouter>
-      <Checkout />
+      <ShopOutletContext.Provider value={{ shoppingCartItems: [], getItem: () => {} }}>
+        <Checkout />
+      </ShopOutletContext.Provider>
     </BrowserRouter>
   );
 
